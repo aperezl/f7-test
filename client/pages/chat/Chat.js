9dec9e6a7adf8608862d7b4c639d14dd1563d63b
Antonio Manuel Pérez López <aperezl@gmail.com>
2016-06-07 23:25:15 +0200

    Changed styles

--- client/pages/chat/Chat.js
+++ client/pages/chat/Chat.js
@@ -1,7 +1,50 @@
 Template.Chat.onRendered(function() {
-  console.log('Iniciando...');
-  console.log('f7', f7),
-  f7.messagebar('.messagebar', {
+
+  myMessagebar = f7.messagebar('.messagebar', {
     maxHeight: 200
-  });  
+  });
+
+  myMessages = f7.messages('.messages', {
+    autoLayout:true
+  });
+})
+
+function addMessage() {
+  var messageText = myMessagebar.value().trim();
+  if (messageText.length === 0) return;
+  myMessagebar.clear()
+  var messageType = (['sent', 'received'])[Math.round(Math.random())];
+  var avatar, name;
+  if(messageType === 'received') {
+    avatar = 'http://lorempixel.com/200/200/people/9';
+    name = 'Kate';
+  }
+
+  var conversationStarted = false;
+  myMessages.addMessage({
+    text: messageText,
+    type: messageType,
+    avatar: avatar,
+    name: name,
+    day: !conversationStarted ? 'Today' : false,
+    time: !conversationStarted ? (new Date()).getHours() + ':' + (new Date()).getMinutes() : false
+  });
+
+  conversationStarted = true;
+}
+
+
+Template.Chat.events({
+  'click .messagebar .link': function(event, instance) {
+    addMessage();
+
+  },
+  'keypress .messagebar': function(event) {
+    if (event.which === 13) {
+         //event.stopPropagation();
+         addMessage();
+         return false;
+      }
+  }
+
 })
